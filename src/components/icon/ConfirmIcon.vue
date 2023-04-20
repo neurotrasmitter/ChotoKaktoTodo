6540f365d3367a9df463fb3a131616f894870f83
Cow Duck <madduckoff@gmail.com>
2023-04-20 19:05:28 +0300

    initial commit

---
+++ src/components/icon/ConfirmIcon.vue
@@ -0,0 +1,43 @@
+<template>
+  <svg
+    width="20px"
+    height="20px"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      :style="strokeColor"
+      d="M10.5 10.5L11.5 11.5L14 9M8.25 5H15.75C16.4404 5 17 5.58763 17 6.3125V19L12 15.5L7 19V6.3125C7 5.58763 7.55964 5 8.25 5Z"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: "ConfirmIcon",
+  props: {
+    color: {
+      type: String,
+      default: "#000000",
+    },
+  },
+  computed: {
+    strokeColor() {
+      return `stroke: ${this.color}`;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.path {
+  stroke: black;
+}
+
+path:hover {
+  stroke: red;
+}
+</style>
---
+++ src/components/icon/DiscardIcon.vue
@@ -0,0 +1,24 @@
+<template>
+  <svg
+    width="20px"
+    height="20px"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      class="path"
+      d="M10.5 8.56L12 10.06M12 10.06L13.5 11.56M12 10.06L13.5 8.56M12 10.06L10.5 11.56M8.25 5H15.75C16.4404 5 17 5.58763 17 6.3125V19L12 15.5L7 19V6.3125C7 5.58763 7.55964 5 8.25 5Z"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: "DiscardIcon",
+};
+</script>
+
+<style scoped></style>
